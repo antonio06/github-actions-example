72828bb481f4f7eb7f7fd5db2667b831ffee309c
Antonio  Contreras LEMONCODE <antonio.contreras@lemoncode.net>
2021-02-25 19:41:06 +0100

    Updated index component

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ const classes = require("./myStyles.scss");
 
 ReactDOM.render(
   <div>
-    <h1 className={classes.title}>Hello World people</h1>
+    <h1 className={classes.title}>Hello World people to react</h1>
     <span>Env url: {process.env.API_BASE}</span>
     <ImgComponent />
     <TypeComponent />
