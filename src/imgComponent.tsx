c5539b74a43002ab6a08c0568065bc0b5efe020f
Antonio  Contreras LEMONCODE <antonio.contreras@lemoncode.net>
2021-02-25 16:34:06 +0100

    Remove all folders less dist folder

--- src/imgComponent.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-const classes = require("./imgComponent.styles.scss");
-const logoImg = require("./content/logo_1.png");
-
-export const ImgComponent: React.FunctionComponent = () => {
-  return <img className={classes.img} src={logoImg} />;
-};
